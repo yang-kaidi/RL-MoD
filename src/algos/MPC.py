433b617d84f640b704ca6c0fe7af7a7f8274bc07
stillwater1990 <yangkaidi07@gmail.com>
2020-11-25 12:15:48 -0800

    fixing scenario and updating MPC alg codes

--- src/algos/MPC.py
+++ src/algos/MPC.py
@@ -8,15 +8,18 @@ import numpy as np
 import subprocess
 import os
 import networkx as nx
-from util import mat2str
+from src.misc.utils import mat2str
 from copy import deepcopy
 
 
 class MPC:
-    def __init__(self, env, CPLEXPATH=None):
+    def __init__(self, env, CPLEXPATH=None, platform = None):
         self.env = env 
         self.CPLEXPATH = CPLEXPATH
-    def MPC_exact(self, CPLEXPATH=None):
+        if self.CPLEXPATH is None:
+            self.CPLEXPATH = "C:/Program Files/ibm/ILOG/CPLEX_Studio1210/opl/bin/x64_win64/"
+        self.platform = platform
+    def MPC_exact(self):
         t = self.env.time
         demandAttr = [(i,j,tt,self.env.demand[i,j][tt], self.env.price[i,j][tt]) for i,j in self.env.demand for tt in range(t,t+self.env.T) if self.env.demand[i,j][tt]>1e-3]
         accTuple = [(n,self.env.acc[n][t]) for n in self.env.acc]
@@ -37,15 +40,16 @@ class MPC:
             file.write('edgeAttr='+mat2str(edgeAttr)+';\r\n')
             file.write('accInitTuple='+mat2str(accTuple)+';\r\n')
             file.write('daccAttr='+mat2str(daccTuple)+';\r\n')
-            
+        
         modfile = modPath+'MPC.mod'
-        if CPLEXPATH is None:
-            CPLEXPATH = "C:/Program Files/ibm/ILOG/CPLEX_Studio1210/opl/bin/x64_win64/"
         my_env = os.environ.copy()
-        my_env["LD_LIBRARY_PATH"] = CPLEXPATH
+        if self.platform == None:
+            my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        else:
+            my_env["DYLD_LIBRARY_PATH"] = self.CPLEXPATH
         out_file =  MPCPath + 'out_{}.dat'.format(t)
         with open(out_file,'w') as output_f:
-            subprocess.check_call([CPLEXPATH+"oplrun", modfile,datafile],stdout=output_f,env=my_env)
+            subprocess.check_call([self.CPLEXPATH+"oplrun", modfile,datafile],stdout=output_f,env=my_env)
         output_f.close()
         paxFlow = defaultdict(float)
         rebFlow = defaultdict(float)
@@ -87,10 +91,12 @@ class MPC:
             file.write('daccAttr='+mat2str(daccTuple)+';\r\n')
            
         modfile = modPath+'bi-level-matching.mod'
-        if self.CPLEXPATH is None:
-            self.CPLEXPATH = "C:/Program Files/ibm/ILOG/CPLEX_Studio1210/opl/bin/x64_win64/"
+        
         my_env = os.environ.copy()
-        my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        if self.platform == None:
+            my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        else:
+            my_env["DYLD_LIBRARY_PATH"] = self.CPLEXPATH
         out_file =  MPCPath + 'out_{}.dat'.format(t)
         with open(out_file,'w') as output_f:
             subprocess.check_call([self.CPLEXPATH+"oplrun", modfile,datafile],stdout=output_f,env=my_env)
@@ -135,10 +141,11 @@ class MPC:
             file.write('daccAttr='+mat2str(daccTuple)+';\r\n')
            
         modfile = modPath+'bi-level-rebalancing.mod'
-        if self.CPLEXPATH is None:
-            self.CPLEXPATH = "C:/Program Files/ibm/ILOG/CPLEX_Studio1210/opl/bin/x64_win64/"
         my_env = os.environ.copy()
-        my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        if self.platform == None:
+            my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        else:
+            my_env["DYLD_LIBRARY_PATH"] = self.CPLEXPATH
         out_file =  MPCPath + 'out_{}.dat'.format(t)
         with open(out_file,'w') as output_f:
             subprocess.check_call([self.CPLEXPATH+"oplrun", modfile,datafile],stdout=output_f,env=my_env)
@@ -190,10 +197,12 @@ class MPC:
             file.write('daccAttr='+mat2str(daccTuple)+';\r\n')
            
         modfile = modPath+'bi-level-rebalancing-2-actions.mod'
-        if self.CPLEXPATH is None:
-            self.CPLEXPATH = "C:/Program Files/ibm/ILOG/CPLEX_Studio1210/opl/bin/x64_win64/"
+        
         my_env = os.environ.copy()
-        my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        if self.platform == None:
+            my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        else:
+            my_env["DYLD_LIBRARY_PATH"] = self.CPLEXPATH
         out_file =  MPCPath + 'out_{}.dat'.format(t)
         with open(out_file,'w') as output_f:
             subprocess.check_call([self.CPLEXPATH+"oplrun", modfile,datafile],stdout=output_f,env=my_env)
@@ -249,10 +258,12 @@ class MPC:
             file.write('daccAttr='+mat2str(daccTuple)+';\r\n')
            
         modfile = modPath+'tri-level.mod'
-        if self.CPLEXPATH is None:
-            self.CPLEXPATH = "C:/Program Files/ibm/ILOG/CPLEX_Studio1210/opl/bin/x64_win64/"
+        
         my_env = os.environ.copy()
-        my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        if self.platform == None:
+            my_env["LD_LIBRARY_PATH"] = self.CPLEXPATH
+        else:
+            my_env["DYLD_LIBRARY_PATH"] = self.CPLEXPATH
         out_file =  MPCPath + 'out_{}.dat'.format(t)
         with open(out_file,'w') as output_f:
             subprocess.check_call([self.CPLEXPATH+"oplrun", modfile,datafile],stdout=output_f,env=my_env)
@@ -280,43 +291,3 @@ class MPC:
                         desiredAcc[int(i)] = float(f)
         return desiredAcc
 
-def solveRebFlow(env,res_path,desiredAcc,CPLEXPATH):
-    t = env.time
-    accRLTuple = [(n,desiredAcc[n]) for n in desiredAcc]
-    accTuple = [(n,int(env.acc[n][t])) for n in env.acc]
-    edgeAttr = [(i,j,env.G.edges[i,j]['time']) for i,j in env.G.edges]
-    modPath = os.getcwd().replace('\\','/')+'/mod/'
-    OPTPath = os.getcwd().replace('\\','/')+'/MPC/'+res_path
-    if not os.path.exists(OPTPath):
-        os.makedirs(OPTPath)
-    datafile = OPTPath + f'data_{t}.dat'
-    resfile = OPTPath + f'res_{t}.dat'
-    with open(datafile,'w') as file:
-        file.write('path="'+resfile+'";\r\n')
-        file.write('edgeAttr='+mat2str(edgeAttr)+';\r\n')
-        file.write('accInitTuple='+mat2str(accTuple)+';\r\n')
-        file.write('accRLTuple='+mat2str(accRLTuple)+';\r\n')
-    modfile = modPath+'minRebDistRebOnly.mod'
-    if CPLEXPATH is None:
-        CPLEXPATH = "/opt/ibm/ILOG/CPLEX_Studio128/opl/bin/x86-64_linux/"
-    my_env = os.environ.copy()
-    my_env["LD_LIBRARY_PATH"] = CPLEXPATH
-    out_file =  OPTPath + f'out_{t}.dat'
-    with open(out_file,'w') as output_f:
-        subprocess.check_call([CPLEXPATH+"oplrun", modfile, datafile], stdout=output_f, env=my_env)
-    output_f.close()
-
-    # 3. collect results from file
-    flow = defaultdict(float)
-    with open(resfile,'r', encoding="utf8") as file:
-        for row in file:
-            item = row.strip().strip(';').split('=')
-            if item[0] == 'flow':
-                values = item[1].strip(')]').strip('[(').split(')(')
-                for v in values:
-                    if len(v) == 0:
-                        continue
-                    i,j,f = v.split(',')
-                    flow[int(i),int(j)] = float(f)
-    action = [flow[i,j] for i,j in env.edges]
-    return action
